9ab9a07c7f656e885695d2865f8deb1fc9d21ad1
Krishna <balkrishna.kadam@global-csg.com>
2025-02-13 09:58:54 +0530

    power bi url update

--- src/config.js
+++ src/config.js
@@ -3,17 +3,17 @@ const configurations = {
     development: {
         API_BASE_URL: process.env.REACT_APP_API_URL || 'http://localhost:8000',
         WEBSOCKET_URL: process.env.REACT_APP_WEBSOCKET_URL || 'ws://localhost:8000/ws',
-        POWER_BI_URL: process.env.REACT_APP_POWER_BI_URL || 'https://app.powerbi.com/reportEmbed?reportId=859cf7be-971b-41a5-a2b3-af1991e4be54&autoAuth=true&ctid=a9c50c6c-2ecc-4653-99b2-58024af91866&navContentPaneEnabled=false&filterPaneEnabled=false'
+        POWER_BI_URL: process.env.REACT_APP_POWER_BI_URL || 'https://app.powerbi.com/reportEmbed?reportId=3a844791-90c2-4130-bc48-5f7ed25e2214&autoAuth=true&ctid=a9c50c6c-2ecc-4653-99b2-58024af91866&navContentPaneEnabled=false&filterPaneEnabled=false'
     },
     production: {
         API_BASE_URL: process.env.REACT_APP_API_URL || 'http://172.16.117.87:8000',
         WEBSOCKET_URL: process.env.REACT_APP_WEBSOCKET_URL || 'ws://172.16.117.87:8000/ws',
-        POWER_BI_URL: process.env.REACT_APP_POWER_BI_URL || 'https://app.powerbi.com/reportEmbed?reportId=859cf7be-971b-41a5-a2b3-af1991e4be54&autoAuth=true&ctid=a9c50c6c-2ecc-4653-99b2-58024af91866&navContentPaneEnabled=false&filterPaneEnabled=false'
+        POWER_BI_URL: process.env.REACT_APP_POWER_BI_URL || 'https://app.powerbi.com/reportEmbed?reportId=3a844791-90c2-4130-bc48-5f7ed25e2214&autoAuth=true&ctid=a9c50c6c-2ecc-4653-99b2-58024af91866&navContentPaneEnabled=false&filterPaneEnabled=false'
     }
 };
 
 
-const env = 'production';
+const env = 'production'; 
 const config = configurations[env];
 
 // Fallback logic
